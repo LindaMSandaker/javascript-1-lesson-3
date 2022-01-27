dd70336d03cf4f42c57ce8ae6d58acc6ce3fd392
Linda Sandaker <linda.m.sandaker@gmail.com>
2022-01-27 13:27:52 +0100

    from lesson 27.01.22

--- 3-creating-HTML-from-an-array-of-objects/js/petList.js
+++ 3-creating-HTML-from-an-array-of-objects/js/petList.js
@@ -17,8 +17,37 @@ const pets = [
         friendly: false,
     },
 ];
+
 // Select the pet-container class
 
-// Create a html variable to hold our htm in it when we use the loop
+const container = document.querySelector(".pet-container");
+
+// Create a html variable to hold our html in it when we use the loop
+
+let html = "";
+
 // Loop over the array
+
+for (let i = 0; i < pets.length; i++) {
+
+    let theType = "";
+
+    if (pets[i].type) { // if (pets[i].type !==null)
+        theType = pets[i].type;
+    } else {
+        theType = "Unknown type";
+    }
+
+    html = html + `
+    <div>
+        <h4> Type: ${theType} </h4>
+        <p> Age: ${pets[i].age}</p>
+        <p> Friendly: ${pets[i].friendly ? "Yes, it's friendly." : "No, it's not friendly."}</p>
+    </div >
+    `;
+    console.log(html);
+}
+
 // change the innerHTML
+
+container.innerHTML = html;
